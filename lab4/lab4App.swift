a26e9b18adb2233cf78e817ed947d55a213a5974
joseph loveall <jploveal@asu.edu>
2026-03-02 12:35:35 -0700

    Initial Commit

---
+++ lab4/lab4App.swift
@@ -0,0 +1,17 @@
+//
+//  lab4App.swift
+//  lab4
+//
+//  Created by Joseph Loveall (Student) on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct lab4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
